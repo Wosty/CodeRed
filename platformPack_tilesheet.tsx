379a1e3ed8dccfe9e91a3381660d6a22219412de
lwcreel <landon.creel@icloud.com>
2018-11-03 17:17:44 -0500

    tilesheet and demo level

---
+++ platformPack_tilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="platformPack_tilesheet" tilewidth="32" tileheight="32" tilecount="392" columns="28">
+ <image source="../../../Downloads/kenney_simplifiedplatformer/Tilesheet/platformPack_tilesheet.png" width="896" height="448"/>
+</tileset>
